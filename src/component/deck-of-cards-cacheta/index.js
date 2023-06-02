70ef73821a86ba911f5d14b717edc12df12c6c9a
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-06-02 13:23:38 -0300

    parta do jogo cacheta concluido

--- src/component/deck-of-cards-cacheta/index.js
+++ src/component/deck-of-cards-cacheta/index.js
@@ -1,25 +1,134 @@
-import { BotaoEmbaralharDeck } from "../botoes/btnEmbaralharDeck"
-import { BotaoPaginaInicial } from "../botoes/btnPaginaInicial"
+import { useEffect, useState } from "react";
+import { BotaoEmbaralharDeck } from "../botoes/btnEmbaralharDeck";
+import { BotaoPaginaInicial } from "../botoes/btnPaginaInicial";
 
-import "./index.css"
+import "./index.css";
 
-// cachetaCards: [
-//     data.cards[0], data.cards[1], data.cards[2], 
-//     data.cards[3], data.cards[4], data.cards[5], 
-//     data.cards[6], data.cards[7], data.cards[8]
-//   ],
+async function createDeck() {
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
+  );
+  const deck = await response.json();
+  return deck.deck_id;
+}
+
+async function getCards(deckId) {
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
+  );
+  return await response.json();
+}
+
+async function reshuffleCards(deckId) {
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`
+  );
+  return await response.json();
+}
+
+const posicoesDoBaralho = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const criandoMaoDoJogo = () => {
+  const posicoes = [];
+  let a = 0;
+  while (a < 9) {
+    let item = posicoesDoBaralho(0, 52);
+    if (posicoes.indexOf(item) === -1) {
+      posicoes.push(item);
+      a++;
+    }
+  }
+  return posicoes;
+};
 
 export const DeckOfCardsCacheta = () => {
-    return(
-        <div className="sectionContainerCacheta">
-            <h2 className="tituloCacheta">Sua mão para jogar Cacheta</h2>
+  const [deck, setDeck] = useState({
+    cards: [],
+    deck_id: String,
+    cachetaCards: [],
+  });
 
-            {/* Voltando as cartas no número certo para o jogo */}
+  useEffect(() => {
+    const fetchData = async () => {
+      const deckId = await createDeck();
+      const data = await getCards(deckId);
+      const posicoes = [criandoMaoDoJogo()];
 
-            <div className="btnContainer">
-                <BotaoEmbaralharDeck />
-                <BotaoPaginaInicial />
-            </div>
-        </div>
-    )
-}
+      setDeck({
+        cards: data.cards,
+        deck_id: data.deck_id,
+        cachetaCards: [
+          data.cards[posicoes[0][0]],
+          data.cards[posicoes[0][1]],
+          data.cards[posicoes[0][2]],
+          data.cards[posicoes[0][3]],
+          data.cards[posicoes[0][4]],
+          data.cards[posicoes[0][5]],
+          data.cards[posicoes[0][6]],
+          data.cards[posicoes[0][7]],
+          data.cards[posicoes[0][8]],
+        ],
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  const embaralharDeck = async (deckId) => {
+    await reshuffleCards(deckId);
+    const data = await getCards(deckId);
+    const posicoes = [criandoMaoDoJogo()];
+
+    setDeck({
+      cards: data.cards,
+      deck_id: data.deck_id,
+      cachetaCards: [
+        data.cards[posicoes[0][0]],
+        data.cards[posicoes[0][1]],
+        data.cards[posicoes[0][2]],
+        data.cards[posicoes[0][3]],
+        data.cards[posicoes[0][4]],
+        data.cards[posicoes[0][5]],
+        data.cards[posicoes[0][6]],
+        data.cards[posicoes[0][7]],
+        data.cards[posicoes[0][8]],
+      ],
+    });
+  };
+
+  return (
+    <div className="sectionContainerCacheta">
+      <h2 className="tituloCacheta">Sua mão para jogar Cacheta</h2>
+
+      <ul className="cartasCacheta">
+        {deck.cachetaCards.map((cardCacheta, index) => {
+          return (
+            <li key={index}>
+              <img src={cardCacheta.image} alt={cardCacheta.value} />
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="btnContainer">
+        <BotaoEmbaralharDeck
+          embaralharDeck={embaralharDeck}
+          deckId={deck.deck_id}
+        />
+        <BotaoPaginaInicial />
+      </div>
+
+      <ul className="cartasBaralhoContainer">
+        {deck.cards.map((card, index) => {
+          return (
+            <li key={index}>
+              <img src={card.image} alt={card.value} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
--- src/component/deck-of-cards-truco/index.js
+++ src/component/deck-of-cards-truco/index.js
@@ -1,49 +1,53 @@
 import { useState, useEffect } from "react";
 
-import "./index.css"
+import "./index.css";
 
 import { BotaoPaginaInicial } from "../botoes/btnPaginaInicial";
 import { BotaoEmbaralharDeck } from "../botoes/btnEmbaralharDeck";
 
 async function createDeck() {
-  const response = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
+  );
   const deck = await response.json();
   return deck.deck_id;
 }
 
 async function getCards(deckId) {
-  const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`);
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
+  );
   return await response.json();
 }
 
 async function reshuffleCards(deckId) {
-  const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`
+  );
   return await response.json();
 }
 
 const posicoesDoBaralho = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min)
-}
-
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 const criandoMaoDoJogo = () => {
-  const posicoes = []
-  let a = 0
-      while (a < 3){
-        let item = posicoesDoBaralho(0, 52)
-        if (posicoes.indexOf(item) === -1){
-          posicoes.push(item)
-          a++
-        }
-      }
-  return posicoes
-}
+  const posicoes = [];
+  let a = 0;
+  while (a < 3) {
+    let item = posicoesDoBaralho(0, 52);
+    if (posicoes.indexOf(item) === -1) {
+      posicoes.push(item);
+      a++;
+    }
+  }
+  return posicoes;
+};
 
 export const DeckOfCardsTruco = () => {
   const [deck, setDeck] = useState({
     cards: [],
     trucoCards: [],
-    cachetaCards: [],
     deck_id: String,
   });
 
@@ -51,19 +55,15 @@ export const DeckOfCardsTruco = () => {
     const fetchData = async () => {
       const deckId = await createDeck();
       const data = await getCards(deckId);
-      const posicoes = [criandoMaoDoJogo()]
-      console.log(data);
-      console.log(posicoes[0]);
-      
-
+      const posicoes = [criandoMaoDoJogo()];
       setDeck({
         cards: data.cards,
+        deck_id: data.deck_id,
         trucoCards: [
-          data.cards[posicoes[0][0]], 
-          data.cards[posicoes[0][1]], 
-          data.cards[posicoes[0][2]]
+          data.cards[posicoes[0][0]],
+          data.cards[posicoes[0][1]],
+          data.cards[posicoes[0][2]],
         ],
-        deck_id: data.deck_id
       });
     };
 
@@ -72,26 +72,23 @@ export const DeckOfCardsTruco = () => {
 
   const embaralharDeck = async (deckId) => {
     await reshuffleCards(deckId);
-    const data = await getCards(deckId)
-    const posicoes = [criandoMaoDoJogo()]
-    console.log(data);
-    console.log(posicoes[0]);
+    const data = await getCards(deckId);
+    const posicoes = [criandoMaoDoJogo()];
     setDeck({
       cards: data.cards,
       deck_id: data.deck_id,
       trucoCards: [
-        data.cards[posicoes[0][0]], 
-        data.cards[posicoes[0][1]], 
-        data.cards[posicoes[0][2]]
+        data.cards[posicoes[0][0]],
+        data.cards[posicoes[0][1]],
+        data.cards[posicoes[0][2]],
       ],
-    })
-  }
+    });
+  };
 
   // tive q criar uma função para subscrever o estado inicial sem criar outro deckId
 
   return (
     <section className="sectionContainerTruco">
-
       <h2 className="tituloTruco">Sua mão para jogar Truco</h2>
 
       <ul className="cartasTruco">
@@ -100,12 +97,15 @@ export const DeckOfCardsTruco = () => {
             <li key={index}>
               <img src={cardTruco.image} alt={cardTruco.value} />
             </li>
-          )
+          );
         })}
       </ul>
 
       <div className="btnContainer">
-        <BotaoEmbaralharDeck embaralharDeck={embaralharDeck} deckId={deck.deck_id} />
+        <BotaoEmbaralharDeck
+          embaralharDeck={embaralharDeck}
+          deckId={deck.deck_id}
+        />
         <BotaoPaginaInicial />
       </div>
 
